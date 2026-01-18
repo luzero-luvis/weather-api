a34be50aa69b127d996885fd9b2a204fc4addbda
luvis-sirpi <luvis@sirpi.io>
2026-01-18 23:29:11 +0530

    feat: added healthz and routes

--- main.go
+++ main.go
@@ -10,10 +10,19 @@ import (
 
 func main() {
 	r := chi.NewRouter()
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("welcome"))
+	r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("ok"))
 		log.Println("server is running")
 	})
+
+	r.Route("/api/v1", func(r chi.Router) {
+		r.Get("/weather", func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("this is weather api"))
+		})
+	})
+
 	if err := http.ListenAndServe(":8001", r); err != nil {
 		fmt.Println("error starting", err)
 	}
