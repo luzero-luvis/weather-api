c67c9f1bf0e20ce303bc4cacda594bd8ffa499e2
luvis-sirpi <luvis@sirpi.io>
2026-02-17 16:21:08 +0530

    chore: added redis related env's

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,30 +3,39 @@ package config
 import (
 	"log/slog"
 	"os"
+	"strconv"
 
 	"github.com/godotenv/godotenv"
 )
 
 type Config struct {
-	APIKey  string
-	Port    string
-	BaseURL string
-	Env     string
+	APIKey    string
+	Port      string
+	BaseURL   string
+	Env       string
+	RedisAddr string
+	RedisPass string
+	RedisDB   int
 }
 
 func Load() (*Config, error) {
 	godotenv.Load()
 
+	redisDB, _ := strconv.Atoi(os.Getenv("DB"))
 	conf := &Config{
-		APIKey:  os.Getenv("API_KEY"),
-		Port:    os.Getenv("PORT"),
-		BaseURL: os.Getenv("BASE_URL"),
-		Env:     os.Getenv("ENV"),
+		APIKey:    os.Getenv("API_KEY"),
+		Port:      os.Getenv("PORT"),
+		BaseURL:   os.Getenv("BASE_URL"),
+		Env:       os.Getenv("ENV"),
+		RedisAddr: os.Getenv("ADDR"),
+		RedisPass: os.Getenv("REDIS_PASS"),
+		RedisDB:   redisDB,
 	}
 
 	slog.Info("configuration loaded",
 		"port", conf.Port,
 		"env", conf.Env,
+		"RedisAddr", conf.RedisAddr,
 	)
 	return conf, nil
 }
